a880078f50ff1607d20bc4872d643fa67cf8924e
DjayaPamungkas21 <djayapamungkas21@gmail.com>
2021-02-25 15:26:33 +0700

    belajar php

---
+++ belajar.php
@@ -0,0 +1,12 @@
+<?php
+   
+    define ('PHI', 3,14);
+
+    $jari_jari = 10;
+    $jari_jari2 = 14;
+    $luas_lingkaran = PHI * $jari_jari * $jari_jari2;
+    echo 'luas lingkaran adalah :'.$luas_lingkaran;
+
+    
+
+?>
